ded27f98e2798e89164a051455347a4781d74cfb
장수민 <jsm0726@naver.com>
2023-07-25 10:10:40 +0900

    solve the 덧칠하기 problem in programmers

---
+++ programmers/Main_덧칠하기.java
@@ -0,0 +1,17 @@
+// Lv1
+
+class Main_덧칠하기 {
+    public int solution(int n, int m, int[] section) {
+        int result = 0;
+        result++;
+        int start = section[0];
+        
+        for (int el : section) {
+            if (el < start + m) continue;
+            start = el;
+            result++;
+        }
+        
+        return result;
+    }
+}
