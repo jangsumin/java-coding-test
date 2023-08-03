8438b4f7dbb2002f097c08dfb9460096dee6c990
장수민 <jsm0726@naver.com>
2023-08-03 17:24:15 +0900

    rename Main_1289.java to Main_12891.java

--- baekjoon/Main_12891.java
+++ baekjoon/Main_12891.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_1289 {
+public class Main_12891 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
