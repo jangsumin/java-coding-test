41953a550ce6f73f6cad3c67ba7269c99dd81da2
장수민 <jsm0726@naver.com>
2023-08-21 17:22:25 +0900

    solve the 타겟 넘버 problem in programmers

---
+++ programmers/Solution_타겟넘버.java
@@ -0,0 +1,20 @@
+pacakage programmers;
+
+class Solution_타겟넘버 {
+    static int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target);
+        return answer;
+    }
+    
+    static void dfs(int cnt, int sum, int[] arr, int target) {
+        if (cnt == arr.length) {
+            if (sum == target) answer++;
+            return;
+        }
+        
+        dfs(cnt + 1, sum + arr[cnt], arr, target);
+        dfs(cnt + 1, sum - arr[cnt], arr, target);
+    }
+}
