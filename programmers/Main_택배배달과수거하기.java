8430955994b3fd601bab6a847c35bc76af4c004d
장수민 <jsm0726@naver.com>
2023-10-03 22:10:49 +0900

    solve the 택배 배달과 수거하기 problem in programmers

---
+++ programmers/Main_택배배달과수거하기.java
@@ -0,0 +1,24 @@
+package programmers
+
+class Main_택배배달과수거하기 {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        int delivery = 0, pickup = 0;
+                                                                           
+        for (int i = n - 1; i >= 0; i--) {
+            if (deliveries[i] > 0 || pickups[i] > 0) {
+                int cnt = 0;
+                while (delivery < deliveries[i] || pickup < pickups[i]) {
+                    cnt++;
+                    delivery += cap;
+                    pickup += cap;
+                }
+                delivery -= deliveries[i];
+                pickup -= pickups[i];
+                answer += 2 * (i + 1) * cnt;
+            }
+        }
+        
+        return answer;
+    }
+}
