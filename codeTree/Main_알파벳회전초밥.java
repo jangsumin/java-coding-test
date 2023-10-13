d44c471dc8abdc3b0867fdc0c758ea93dee9d0e5
장수민 <jsm0726@naver.com>
2023-10-13 22:33:27 +0900

    solve the 알파벳 회전초밥 problem in codeTree

---
+++ codeTree/Main_알파벳회전초밥.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] str = br.readLine().toCharArray();
+        char[] pattern = br.readLine().toCharArray();
+
+        int result = 1;
+        int index = 0;
+        next: while (true) {
+            for (int i = 0; i < str.length; i++) {
+                if (str[i] == pattern[index]) {
+                    index++;
+                    if (index == pattern.length) break next;
+                }
+            }
+            result += 1;
+        }
+
+        System.out.println(result);
+    }
+}
